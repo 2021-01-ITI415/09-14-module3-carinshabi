4a804acfafaace900a093be3654eb089315a7df8
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-04-17 21:28:26 -0400

    Work Session FIXING

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour {
     public AudioClip splashSound;
 
     public AudioSource audioS;
+
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
+    public AudioMixerSnapshot ambIdleSnapshot;
+    public AudioMixerSnapshot ambInSnapshot;
+
+    public LayerMask enemyMask;
+
+    bool enemyNear;
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 10f, transform.forward, 0f, enemyMask);
+        if(hits.Length > 0)
+        {
+            if (!enemyNear)
+            {
+                auxInSnapshot.TransitionTo(0.5f);
+                enemyNear = true;
+            }
+        }
+        else
+        {
+            if(enemyNear)
+            {
+                enemyNear = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,5 +42,28 @@ public class PlayerAudio : MonoBehaviour {
         {
             audioS.PlayOneShot(splashSound);
         }   
+        if (other.CompareTag("EnemyZone"))
+        {
+            auxInSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambInSnapshot.TransitionTo(0.5f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("EnemyZone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            ambIdleSnapshot.TransitionTo(0.5f);
+        }
     }
 }
--- Assets/_Labs/05-Audio/2. Character Stats/Scripts/ScriptableObjects/CharacterStats_SO.cs
+++ Assets/_Labs/05-Audio/2. Character Stats/Scripts/ScriptableObjects/CharacterStats_SO.cs
@@ -272,7 +272,7 @@ public class CharacterStats_SO : ScriptableObject
     public void saveCharacterData()
     {
         saveDataOnClose = true;
-        EditorUtility.SetDirty(this);
+        //EditorUtility.SetDirty(this);
     }
     #endregion
 }
