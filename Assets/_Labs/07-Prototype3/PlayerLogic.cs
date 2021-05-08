afb4883c5a684e39d3f8f0adec2ad6eb55c9b529
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-05-08 14:17:41 -0400

    Work Session Final

---
+++ Assets/_Labs/07-Prototype3/PlayerLogic.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public int ItemsCollected;
+    public GameObject GameOverPanel;
+    public Text StatsText;
+    public int ItemsToCollect;
+    public GameObject Rain;
+    public ParticleSystem[] particles;
+    public AudioSource[] audioSources;
+    public AudioSource playerAudio;
+    public AudioClip collect;
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            ItemsCollected++;
+            Destroy(collision.gameObject);
+        }
+    }*/
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Item")
+        {
+            playerAudio.clip = collect;
+            playerAudio.Play();
+            ItemsCollected++;
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.tag == "End")
+        {
+            GameOverPanel.SetActive(true);
+            StatsText.text = "Items Collected Stats: " + ItemsCollected +"/" + ItemsToCollect ;
+
+        }
+
+        if(other.gameObject.tag == "Building")
+        {
+            Rain.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Building")
+        {
+            Rain.SetActive(true);
+            
+            foreach(ParticleSystem p in particles)
+            {
+                p.Play();
+            }
+
+            foreach(AudioSource audio in audioSources)
+            {
+                audio.Play();
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSources = Rain.GetComponents<AudioSource>();
+        GameOverPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (ItemsCollected >= WinScore)
+        {
+            
+        }*/
+    }
+}
